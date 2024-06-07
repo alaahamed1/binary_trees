0025c51fc777d50fd27534740409242f9b504022
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-06-08 01:17:31 +0300

    Advanced Task-100

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,11 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - Measures the depth of a node in a binary tree
+ * custom_binary_tree_depth - Measures the depth of a node in a binary tree
  * @node: Pointer to the node to measure the depth
+ *
  * Return: Depth of the node
  */
-int binary_tree_depth(const binary_tree_t *node)
+int custom_binary_tree_depth(const binary_tree_t *node)
 {
 	int depth = 0;
 
@@ -15,15 +16,16 @@ int binary_tree_depth(const binary_tree_t *node)
 		node = node->parent;
 	}
 
-	return (depth);
+	return depth;
 }
 
 /**
  * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
  * @first: Pointer to the first node
  * @second: Pointer to the second node
+ *
  * Return: Pointer to the lowest common ancestor node of the two given nodes,
- * or NULL if no common ancestor was found
+ *  or NULL if no common ancestor was found
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
@@ -33,13 +35,12 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 	if (first == second)
 		return ((binary_tree_t *)first);
 
-	int depth_first = binary_tree_depth(first);
-	int depth_second = binary_tree_depth(second);
+	int depth_first = custom_binary_tree_depth(first);
+	int depth_second = custom_binary_tree_depth(second);
 
 	const binary_tree_t *a_first = first;
 	const binary_tree_t *a_second = second;
 
-	/* Equalize the depths */
 	while (depth_first > depth_second)
 	{
 		a_first = a_first->parent;
