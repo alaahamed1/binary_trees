17eae009e1ffc7712cae45804eaedcf818bcdadb
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-06-08 00:36:42 +0300

    Task-18

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -6,8 +6,8 @@
  * @tree: pointer to the node to measure the depth
  * Return:
  */
-
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+	return ((tree && tree->parent) ? 1
+	 + binary_tree_depth(tree->parent) : 0);
 }
